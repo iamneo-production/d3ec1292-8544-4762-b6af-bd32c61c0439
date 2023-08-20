d6e8fb2084f77d16aa28da8a55957faa10b6bffa
vinithkumarac2010 <vinithkumarac2010@gmail.com>
2023-08-20 11:04:02 +0000

    Commit

---
+++ demo/src/main/java/com/example/team15/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.team15.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
